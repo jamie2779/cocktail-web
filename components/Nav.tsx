483cc84e17276570740f944f4d8c8a0d31299ad1
LAPLACE4A <laplace47594a@gmail.com>
2024-11-20 11:05:31 +0900

    fix: nav bar position

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,12 +5,18 @@ import Image from "next/image";
 const Nav = () => {
   return (
     <Box
+      position="fixed"
+      width="402px"
+      zIndex={999}
+      left="50%"
+      transform="translateX(-50%)"
+      bottom={0}
+      h="60px"
       display="flex"
       justifyContent="space-around"
       padding="12px"
       backgroundColor="#fff"
-      borderRadius="8px"
-      boxShadow="0 -2px 8px rgba(0, 0, 0, 0.1)"
+      boxShadow="0px 0px 30px 0px rgba(0, 0, 0, 0.1)"
     >
       <NavItem
         icon={<Image src="/Drink.svg" alt="Menu" width={24} height={24} />}
